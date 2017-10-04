d129cbf2da1b31199b72a0cb030c6142c1652337
steve <sharshman193@gmail.com>
2017-10-04 14:20:43 -0600

    eda

--- sandbox.py
+++ sandbox.py
@@ -11,6 +11,12 @@ def max_tank(df):
             max_val = df[df['tankCode'] == tank].shape[0]
     return df[df['tankCode'] == tank_code]
 
+def water_comp(df):
+    return df[df['newWaterInventory'].notnull()]
+
+def ticket_org(df):
+    return df.sort_values('runTicketStartDate')
+
 
 if __name__ == '__main__':
     enb_df = pickle.load(open('data/enb.pkl', 'rb'))
@@ -19,3 +25,7 @@ if __name__ == '__main__':
     ticket_df.drop_duplicates(inplace=True)
 
     tank_df = max_tank(enb_df)
+    tank_df = water_comp(tank_df)
+
+    tank_ticket = max_tank(ticket_df)
+    tank_ticket = ticket_org(tank_ticket)
