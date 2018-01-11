517ca709c8269a37a8ab3a6339389d20dd167340
Steve <sharshman193@gmail.com>
2018-01-11 15:11:27 -0700

    overcalculation comparison

--- fluids_comp.py
+++ fluids_comp.py
@@ -24,6 +24,7 @@ def lgr_pull():
               ,LGR.TotalWaterOnSite AS LGRWater
               ,LGR.FacilityCapacity
               ,LGR.CalcDate
+              ,LGR.PredictionMethod
         FROM [TeamOptimizationEngineering].[dbo].[InventoryAll] AS LGR
         JOIN (SELECT	FacilityKey
         				,MAX(CalcDate) maxtime
@@ -143,6 +144,44 @@ def gwr_pull():
 
     return new_df.drop_duplicates()
 
+def ticket_pull():
+    try:
+        connection = pyodbc.connect(r'Driver={SQL Server Native Client 11.0};'
+                                    r'Server=SQLDW-L48.BP.Com;'
+                                    r'Database=EDW;'
+                                    r'trusted_connection=yes'
+                                    )
+    except pyodbc.Error:
+    	print("Connection Error")
+    	sys.exit()
+
+    cursor = connection.cursor()
+    SQLCommand = ("""
+        SELECT	RT.assetId
+                ,
+        FROM [EDW].[Enbase].[RunTicket] AS RT
+        JOIN (SELECT	TAG_PREFIX
+        				,MAX(CalcDate) maxtime
+        	FROM [TeamOptimizationEngineering].[Reporting].[PI_Tanks]
+        	GROUP BY TAG_PREFIX, DAY(CalcDate), MONTH(CalcDate), YEAR(CalcDate)) AS MD
+        ON	MD.TAG_PREFIX = PIT.TAG_PREFIX
+        AND	MD.maxtime = PIT.CalcDate
+        JOIN [TeamOptimizationEngineering].[Reporting].[PITag_Dict] AS PTD
+	        ON PTD.TAG = PIT.TAG_PREFIX;
+    """)
+
+    cursor.execute(SQLCommand)
+    results = cursor.fetchall()
+
+    df = pd.DataFrame.from_records(results)
+    connection.close()
+
+    try:
+    	df.columns = pd.DataFrame(np.matrix(cursor.description))[0]
+    except:
+    	df = None
+    	print('Dataframe is empty')
+
 def data_link(lgr, gwr):
     return lgr.merge(gwr, how='outer', on=['API', 'CalcDate'])
 
@@ -167,7 +206,28 @@ def plot_lgr(df, variable='oil', plot_type='hist'):
         labels = ['Oil', 'Water']
         ax.set_xticklabels(labels)
 
-    plt.savefig('images/lgr_{}_{}'.format(plot_type, variable))
+    plt.savefig('images/lgr_{}_{}.png'.format(plot_type, variable))
+
+def lgr_over(df):
+    plt.close()
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+
+    v4_dic = {}
+    v2_dic = {}
+    for date in df['CalcDate'].unique():
+        v2_dic[date] = df[(df['CalcDate'] == date) & (df['PredictionMethod'] == 'LGRv2')].shape[0]
+        v4_dic[date] = df[(df['CalcDate'] == date) & (df['PredictionMethod'] == 'LGRv4')].shape[0]
+
+    width = 0.8
+    v2 = ax.bar(list(v2_dic.keys()), list(v2_dic.values()), width, color='#37b782', tick_label='Version 2')
+    v4 = ax.bar(list(v4_dic.keys()), list(v4_dic.values()), width, color='#1d7f56', tick_label='Version 4')
+
+    plt.title('LGR Predictions Over Facility Capacity by Version')
+    ax.set_xticklabels(tuple(v2_dic.keys()), rotation='vertical')
+    plt.xlabel('Date')
+    plt.ylabel('Count of Wells')
+    plt.legend((v2[0], v4[0]), ('Version 2', 'Version 4'))
+    plt.savefig('images/lgr_over_version.png')
 
 
 if __name__ == '__main__':
@@ -177,12 +237,14 @@ if __name__ == '__main__':
     # df_gwr.to_csv('data/gwr.csv')
 
     df_lgr = pd.read_csv('data/lgr.csv')
-    df_gwr = pd.read_csv('data/gwr.csv')
+    # df_gwr = pd.read_csv('data/gwr.csv')
 
-    plot_lgr(df_lgr[(df_lgr['LGROil'] <= df_lgr['FacilityCapacity']) & \
-                    (df_lgr['LGROil'] > 0)], variable='oil', plot_type='hist')
+    # plot_lgr(df_lgr[(df_lgr['LGROil'] <= df_lgr['FacilityCapacity']) & \
+    #                 (df_lgr['LGROil'] > 0)], variable='oil', plot_type='hist')
     # plot_lgr(df_lgr[(df_lgr['LGROil'] > df_lgr['FacilityCapacity']) & \
     #                 (df_lgr['LGROil'] < 1000)], variable='oil', plot_type='hist')
+
+    lgr_over(df_lgr[df_lgr['LGROil'] > df_lgr['FacilityCapacity']])
 
     # df = data_link(df_lgr, df_gwr)
     # df.to_csv('data/linked_df.csv')
