4275eb772301666694de1e23d844a73bb6589aaa
Steve <sharshman193@gmail.com>
2018-04-09 15:10:06 -0600

    getting archive snapshot values

--- PIthon.py
+++ PIthon.py
@@ -12,15 +12,40 @@ from OSIsoft.AF.Time import *
 from System.Net import NetworkCredential
 
 def connect_to_Server(serverName):
-    piServers = PIServers()
-    global piServer
-    piServer = piServers[serverName]
+	piServers = PIServers()
+	global piServer
+	piServer = piServers[serverName]
 
-    cred = NetworkCredential('piuser','')
+	cred = NetworkCredential('piuser','')
 
-    piServer.Connect(cred)
+	piServer.Connect(cred)
+
+	tag = PIPoint.FindPIPoint(piServer, "sinusoid")
+	lastData = tag.Snapshot()
+
+	print('Snapshot Value:\nTimestamp: {0} Value: {1}\n\n'.format(lastData.Timestamp, lastData.Value))
+
+	startTime = AFTime.Now
+	count = 10
+	forward = False;
+	boundary = AFBoundaryType.Inside
+	filter = ""
+	includeFiltered = True
+
+	archiveData = tag.RecordedValuesByCount(startTime, count, forward, boundary, filter, includeFiltered)
+
+	print('Last 10 Archive Values:\n')
+	for item in archiveData :
+		print('Timestamp: {0} Value: {1}'.format(item.Timestamp, item.Value))
+
 
 def get_tag_snapshot(tagname):
-    tag = PIPoint.FindPIPoint(piServer, tagname)
-    lastData = tag.Snapshot()
-    return lastData.Value, lastData.Timestamp
+	tag = PIPoint.FindPIPoint(piServer, tagname)
+	lastData = tag.Snapshot()
+	return lastData.Value, lastData.Timestamp
+
+
+if __name__ == '__main__':
+	PIthon.connect_to_Server('149.179.68.101')
+	value, timestamp = PIthon.get_tag_snapshot('sinusoid')
+	print('Timestamp: {0} Value: {1}'.format(timestamp, value))
