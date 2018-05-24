8e45b0b46d83cd903261977d2f6765ba94758861
Steve <sharshman193@gmail.com>
2018-05-24 11:11:36 -0600

    pulling turbines and sending to SQL

--- get_gwr.py
+++ get_gwr.py
@@ -4,7 +4,7 @@ import csv
 import pandas as pd
 import urllib
 import sqlalchemy
-from tqdm import tqdm
+from datetime import date, timedelta
 from joblib import Parallel
 from joblib import delayed
 from PIthon import *
@@ -12,7 +12,23 @@ from PIthon import *
 connect_to_Server('149.179.68.101')
 
 
-def pullTag(tag, days='-1d', end = 't'):
+def pullTag(tag, days='-1d', end='t'):
+    try:
+        i = tag
+        prefix = i.split('.')[0]
+        tank = i.split('.')[1]
+        print('[+] Pulling ', i)
+
+        # results = get_tag_values(i, days, 't')
+        results = get_tag_interpolate(i, days, end, minutes=15)
+        final = []
+        for j in results:
+            final.append([prefix, tank] + j)
+        return final
+    except:
+        pass
+
+def pullTurbine(tag, days='-1d', end='t'):
     try:
         i = tag
         prefix = i.split('.')[0]
@@ -41,6 +57,45 @@ def pullQuery(pull):
         row = cursor.fetchone()
     return results
 
+def tag_pull():
+    try:
+        connection = pyodbc.connect(r'Driver={SQL Server Native Client 11.0};'
+                                    r'Server=SQLDW-L48.BP.Com;'
+                                    r'Database=TeamOptimizationEngineering;'
+                                    r'trusted_connection=yes'
+                                    )
+    except pyodbc.Error:
+        print("Connection Error")
+        sys.exit()
+
+    cursor = connection.cursor()
+    SQLCommand = ("""
+        SELECT  P.TAG
+                ,W.API
+        FROM [TeamOptimizationEngineering].[Reporting].[PITag_Dict] P
+        INNER JOIN [OperationsDataMart].[Dimensions].[Wells] W
+            ON W.API = P.API
+        WHERE P.API IN ('4903729563', '4903729534', '4903729531', '4903729560',
+                        '4903729561', '4903729555', '4903729556', '4903729582',
+                        '4903729584', '4903729551', '4900724584', '4903729547',
+                        '4903729468', '4903729548', '4903729519', '4903729514');
+	""")
+
+    cursor.execute(SQLCommand)
+    results = cursor.fetchall()
+
+    df = pd.DataFrame.from_records(results)
+    connection.close()
+
+    try:
+        df.columns = pd.DataFrame(np.matrix(cursor.description))[0]
+        df.columns = [col.lower() for col in df.columns]
+    except:
+        df = None
+        print('Dataframe is empty')
+
+    return df
+
 def csvToList(input):
     output = []
     with open(input, "r") as f:
@@ -65,72 +120,28 @@ def sql_push(df, table):
 
     df.to_sql(table, engine, schema='Reporting', if_exists='append', index=False)
 
-def csvToSQL(tableLocation, csvFile, hasHeader=True, delTable=False, calcCol=False):
-    server = '10.75.6.160'
-    cnxn = pyodbc.connect(r'Driver={SQL Server Native Client 11.0};'
-                          r'Server=10.75.6.160;'
-                          r'Database=TeamOptimizationEngineering;'
-						  r'UID=ThundercatIO;'
-						  r'PWD=thund3rc@t10'
-                          )
-    print('[+] Connected to ' + server + '\n   Writing to location ' + tableLocation)
-    with open(csvFile, 'r') as f:
-        reader = csv.reader(f)
-        if hasHeader == True:
-            data = next(reader)
-        query = 'insert into {}'.format(tableLocation) + ' values ({0})'
-        cursor = cnxn.cursor()
-        if delTable == True:
-            cursor.execute('delete from ' + tableLocation)
-        if calcCol == True:
-            import datetime
-            CalcTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-        for data in tqdm(reader):
-            if calcCol == True:
-                data.append(CalcTime)
-            # print(data)
-            try:
-                query = query.format(','.join('?' * len(data)))
-                # print(query)
-                cursor.execute(query, data)
-            except Exception as e:
-                print('Failed on ', query, e)
-        # try:
-        #     cursor.execute(query, data)
-        # except:
-        #     print(data)
-        cursor.commit()
-
-
-if __name__ == '__main__':
-    Tags = csvToList('data/GottenTagNamesGWR.csv')
+def pull_gwr(tags, tag_limit):
     tags_to_pull = []
 
     query = '''
-
-        SELECT Distinct concat([Tag_Prefix],'.',[Tank])
-          FROM [TeamOptimizationEngineering].[Reporting].[North_GWR]
-          with (NoLock)
-          where cast([DateTime] as date)  = cast(getdate()-1 as date)
-
+        SELECT DISTINCT concat([Tag_Prefix],'.',[Tank])
+          FROM [TeamOptimizationEngineering].[Reporting].[GWR_Test]
+          WITH (NoLock)
+          WHERE cast([DateTime] AS DATE)  = cast(getdate()-1 AS DATE)
     '''
 
     alreadyHave = pullQuery(query)
 
-    Tag_limit = ['WAM-CH320C1-160H','WAM-CH533B3_80D','WAM-CL29_150H',\
-                 'WAM-CL29_160H','WAM-CL32_45H','WAM-LM8_115H','WAM-ML11_150H',\
-                 'WAM-ML11_160D','WAM-ML11_160H']
-    for i in Tags[1:]:
+    for i in tags[1:]:
         if i[0] + '.' + i[1] not in alreadyHave:
-            if i[0] in Tag_limit:
+            if i[0] in tag_limit:
                 tags_to_pull.append(i[0] + '.' + i[1])
         else:
             print(i)
 
     gatheredData = [['Tag_Prefix', 'Tank', 'DateTimeStamp', 'Value']]
 
-    testCount = len(Tag_limit)
+    testCount = len(tag_limit)
     alldata = []
     results = Parallel(n_jobs=10)(delayed(pullTag)(i, '-1d') for i in tags_to_pull)
 
@@ -146,3 +157,38 @@ if __name__ == '__main__':
     df.drop_duplicates(inplace=True)
     # df.to_csv('data/gwr_sql.csv', index=False)
     sql_push(df, 'GWR_Test')
+
+def turbine_pull():
+    tag_df = tag_pull()
+    tags = tag_df['tag'].values
+
+    CTS_Tags = [i + '.' + 'CTS_VY' for i in tags] + [i + '.' + 'WAT_VY' for i in tags]
+
+    gatheredData = [['Tag_Prefix', 'Tag', 'DateTimeStamp', 'Value']]
+
+    alldata = []
+    results = Parallel(n_jobs=10)(delayed(pullTurbine)(i, '-2d') for i in CTS_Tags)
+
+    for i in results:
+        try:
+            gatheredData += i
+        except:
+            pass
+
+    listListToCsv(gatheredData, 'data/TurbineDump.csv')
+    df = pd.read_csv('data/TurbineDump.csv')
+    df.rename(index=str, columns={'DateTimeStamp': 'DateTime'}, inplace=True)
+    df.drop_duplicates(inplace=True)
+    df['DateTime'] = pd.to_datetime(df['DateTime']).dt.date
+    df = df.groupby(['Tag_Prefix', 'Tag', 'DateTime'], as_index=False).max()
+    df = df.loc[df['DateTime'] == date.today() - timedelta(1), :]
+    sql_push(df, 'Turbine_Test')
+
+if __name__ == '__main__':
+    tags = csvToList('data/GottenTagNamesGWR.csv')
+    tag_limit = ['WAM-CH320C1-160H','WAM-CH533B3_80D','WAM-CL29_150H',\
+                 'WAM-CL29_160H','WAM-CL32_45H','WAM-LM8_115H','WAM-ML11_150H',\
+                 'WAM-ML11_160D','WAM-ML11_160H']
+
+    # pull_gwr(tags, tag_limit)
+    df = turbine_pull()
